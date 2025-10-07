9a83fba3ffcea6316ce40bbae23284fd83d484b3
Olivia Grace <ograce2@my.bcit.ca>
2025-10-07 14:37:58 -0700

    first

---
+++ StudentsWasm/Models/Constants.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace StudentsWasm.Models;
+
+public class Constants
+{
+    // public const string BASE_URL = "https://mzesf.azurewebsites.net/api";
+    public const string BASE_URL = "/api";
+}
---
+++ StudentsWasm/Models/Student.cs
@@ -0,0 +1,12 @@
+namespace StudentsWasm.Models;
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? School { get; set; }
+    }
